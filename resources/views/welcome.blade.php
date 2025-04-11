5fe2e0ba244588820426909b2e4f7b6da2b852a4
Triada Maria <triadazmaria@gmail.com>
2025-04-11 09:48:06 +0300

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
     <body>
 
         <!--Navigation Bar-->
-        <nav class="navbar navbar-expand-lg my_navbar">
+        <nav class="navbar navbar-expand-lg navbar-acm">
             <div class="container-fluid">
                 <a class="navbar-brand" href="https://www.instagram.com/acmauth" target="_blank">
                     <img src="{{ asset('images/LogoACM.png') }}" alt="Logo" height="50" class="brand-logo me-2">
