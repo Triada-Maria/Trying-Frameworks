ee9da1bdd9fa17e63f6063106b5c006c50666f4f
Triada Maria <triadazmaria@gmail.com>
2025-04-11 14:59:49 +0300

    popup button

---
+++ resources/js/components/PopupButton.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+      <!-- Το κουμπί -->
+      <button class="btn btn-primary" @click="showModal = true">
+        {{ buttonText }}
+      </button>
+  
+      <!-- Το Modal -->
+      <div v-if="showModal" class="modal fade show d-block" tabindex="-1" style="background: rgba(0,0,0,0.5);">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">{{ modalTitle }}</h5>
+              <button type="button" class="btn-close" @click="showModal = false"></button>
+            </div>
+            <div class="modal-body">
+              <slot>
+                <!-- Αν δεν δοθεί περιεχόμενο, δείχνει αυτό -->
+                <p>Περιεχόμενο popup</p>
+              </slot>
+            </div>
+            <div class="modal-footer">
+              <button class="btn btn-secondary" @click="showModal = false">Κλείσιμο</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'PopupButton',
+    props: {
+      buttonText: {
+        type: String,
+        default: 'Click'
+      },
+      modalTitle: {
+        type: String,
+        default: 'Title'
+      }
+    },
+    data() {
+      return {
+        showModal: false
+      };
+    }
+  };
+  </script>
+  
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -5,4 +5,22 @@
         <h2>This is the homepage</h2>
         <h3>Click on "Events"</h3>
     </div>
+    <div>
+        <PopupButton
+      buttonText="Δες πληροφορίες"
+      modalTitle="Πληροφορίες Χρήστη"
+    >
+      <p>Εδώ μπορείς να δεις λεπτομέρειες για τον χρήστη ή ό,τι άλλο θες.</p>
+    </PopupButton>
+    </div>
 </template>
+
+<script>
+import PopupButton from '../components/PopupButton.vue';
+
+export default {
+  components: {
+    PopupButton
+  }
+};
+</script>
